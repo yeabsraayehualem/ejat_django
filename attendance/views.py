153fb8c429ad747025c6151a2a5063c9b7268ca8
Yeab-23 <yeabsira.ayehualem@eaglelionsystems.com>
2026-03-31 08:48:16 +0300

    feat: enable request data parsing and logging in attendance post method

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,4 @@
+from rest_framework.views import APIView
 from django.shortcuts import render
 from rest_framework.response import Response
 from attendance.models import Attendance
@@ -16,7 +17,9 @@ class GetAttendancesView(UserView):
         return Response(data)
     
     def post(self, request):
-        # data = request.data
+        data = request.data
+        print(data)
+        print(request.user)
         attendance = Attendance.objects.create(
             name=request.user,
             status='p'
